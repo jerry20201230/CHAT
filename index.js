3e6fe68f2498fc0618f007d26fd4fab69ae610a3
jerry20201230 <76645282+jerry20201230@users.noreply.github.com>
2022-07-09 11:12:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+const io = require('socket.io')(http,{
+
+ maxHttpBufferSize: 1e8
+});
 const port = process.env.PORT || 3000;
 
 app.get(/js|icon/, (req, res) => {
